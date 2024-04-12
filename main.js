f6d84d5ed7ffe550cdd4f1280f0a709737346b24
Fatal Tempo <109779232+FatalTempo@users.noreply.github.com>
2024-04-12 01:06:12 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+/* intentially empty cause screw module scripts
+
+weight, flow-rate, current conditions and diseases, current medication, 
+Active Problems/Diagnoses, including acute and chronic conditions, diseases, and disorders. Past Medical History (PMH) Past Surgical History (PSH), including surgery dates and reports.
+medical history and clinical data. Information such as demographics, vital signs, diagnoses, surgeries, medications, treatment plans, allergies, laboratory results, radiological studies, immunization
+
+patient's active and past medical history, including immunizations, medical conditions, acute and chronic diseases, testing results, treatments, and more.
+
+
+
+weight, height, medical condition, medical sector (trauma, er), BMI, 
+*/
